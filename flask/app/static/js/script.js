3bc2fb0fcbbddc074645415b50c00ffef426c5e7
Richard Chen <richardchen39@gmail.com>
2016-08-06 23:45:36 -0700

    Add color gradient

--- flask/app/static/js/script.js
+++ flask/app/static/js/script.js
@@ -1,11 +1,12 @@
+var colors = ['#ffffe0','#ffd4ad','#ffa77a','#ff7246','#ff0000'];
+
 $(document).ready(function() {
   $('svg').find('path').each(function(index, province) {
     $(province).on('click', function() {
-      if ($(province).attr('fill') === 'white') {
-        $(province).attr('fill', 'red');
-      } else {
-        $(province).attr('fill', 'white');
-      }
+      var index = parseInt($(province).attr('index'));
+      console.log(index)
+      $(province).attr('fill', colors[(index + 1) % colors.length]);
+      $(province).attr('index', (index + 1) % colors.length);
     });
   });
 });
