165a71faa0161120d8084ea707d13a435a4a3737
Richard Chen <richardchen39@gmail.com>
2016-08-07 01:22:06 -0700

    Add population map

--- flask/app/static/js/script.js
+++ flask/app/static/js/script.js
@@ -1,8 +1,44 @@
 var colors = ['#ffffff', '#ffffe0','#ffd4ad','#ffa77a','#ff7246','#ff0000'];
+var numPrefectures = 34;
+
+var population = {'Beyla': 26,
+'Boffa': 13,
+'Boke': 31,
+'Conakry': 34,
+'Coyah': 15,
+'Dabola': 9,
+'Dalaba': 5,
+'Dinguiraye': 11,
+'Dubreka': 27,
+'Faranah': 18,
+'Forecariah': 14,
+'Fria': 1,
+'Gaoual': 10,
+'Gueckedou': 22,
+'Kankan': 32,
+'Kerouane': 12,
+'Kindia': 30,
+'Kissidougou': 19,
+'Koubia': 2,
+'Koundara': 4,
+'Kouroussa': 16,
+'Labe': 24,
+'Lelouma': 6,
+'Lola': 7,
+'Macenta': 23,
+'Mali': 21,
+'Mamou': 25,
+'Mandiana': 28,
+'Nzerekore': 29,
+'Pita': 17,
+'Siguiri': 33,
+'Telimele': 20,
+'Tougue': 3,
+'Yomou': 8};
 
 $(document).ready(function() {
   $('svg').find('path').each(function(index, province) {
-    $(province).on('click', function() {
+    $(province).click(function() {
       var index = parseInt($(province).attr('index'));
       console.log(index)
       $(province).attr('fill', colors[(index + 1) % colors.length]);
@@ -11,6 +47,10 @@ $(document).ready(function() {
   });
 
   $('#gradient').html(getGradientHTML());
+
+  $('.population').click(function() {
+    createPopulationMap();
+  });
 });
 
 function getGradientHTML() {
@@ -19,4 +59,17 @@ function getGradientHTML() {
     html += '<div style="width: 16.6667%; height: 100%; background: ' + colors[i] + ';"></div>';
   }
   return html;
+}
+
+function createPopulationMap() {
+  $('svg').find('path').each(function(index, province) {
+    var name = $(province).attr('data-name');
+    var count = population[name];
+    $(province).attr('fill', getGradientColor(count));
+  });
+};
+
+function getGradientColor(count) {
+  var intervalSize = numPrefectures / colors.length;
+  return colors[Math.floor((count - 1) / intervalSize)];
 }
