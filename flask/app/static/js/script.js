9ab43ef5a7adc6f3ff18ed5a64b808f8745773c7
Richard Chen <richardchen39@gmail.com>
2016-08-06 21:06:57 -0700

    Fixed jquery shit

--- flask/app/static/js/script.js
+++ flask/app/static/js/script.js
@@ -1,29 +1,8 @@
-function clickEvent() {
-    console.log('hi');
-}
-
 $(document).ready(function() {
-  // TODO - broken
-  // $('svg').find('path').each(function(index, province) { 
-  //  var $province = $(province)
-  //  $province.on('click', function() {
-  //    console.log('hi');
-  //  });
-  // });
-  var paths = document.querySelectorAll('svg path');
-  for (var i = 0; i < paths.length; i++) {
-    (function() {
-      var path = paths[i];
-      path.addEventListener('click', function(event) {
-        event.preventDefault()
-        console.log('here')
-      })
-    })();
-  }
-
-  // .forEach(function(element) {
-  //  element.addEventListener('click', function() {
-  //    console.log('here');
-  //  });
-  // });
+  $('svg').find('path').each(function(index, province) { 
+   var $province = $(province)
+   $province.on('click', function() {
+     console.log('hi');
+   });
+  });
 });
