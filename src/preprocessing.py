73d66900c2121f4d50d2993526f468de9be9a7b6
Lisa Wang <lisa1010@stanford.edu>
2016-08-07 04:25:34 -0700

    finished code for getting data for extrapolation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -16,6 +16,7 @@
 
 import csv
 import numpy as np
+import sys
 
 from sklearn.cross_validation import train_test_split
 
@@ -110,17 +111,20 @@ def clean_data_convert_dates_make_normalize_lat_lon(csv_file, dataset_name):
         for row in clean_rows:
             writer.writerow(row)
 
-
-def convert_clean_csv_to_numpy_for_rnn(clean_csv_file=CLEAN_GUINEA_DATA_PATH, dataset_name="guinea",  num_timesteps=25, case_type = "confirmed cases"):
+def get_data_dict_from_clean_csv(clean_csv_file=CLEAN_GUINEA_DATA_PATH, dataset_name="guinea", case_type="confirmed_cases"):
     data_dict_by_province = defaultdict(list)
-    data = []
-    labels = []
-
     with open(clean_csv_file, "rb") as f:
         rows = csv.reader(f, delimiter=',')
         for row in rows:
             if row[2] == case_type:
                 data_dict_by_province[row[1]].append(row)
+    return data_dict_by_province
+
+
+def convert_clean_csv_to_numpy_for_rnn(clean_csv_file=CLEAN_GUINEA_DATA_PATH, dataset_name="guinea",  num_timesteps=25, case_type = "confirmed cases"):
+    data_dict_by_province = get_data_dict_from_clean_csv(clean_csv_file=clean_csv_file, dataset_name=dataset_name, case_type=case_type)
+    data = []
+    labels = []
 
     for province, rows in data_dict_by_province.iteritems():
         print "processing for province: {}".format(province)
@@ -148,6 +152,90 @@ def convert_clean_csv_to_numpy_for_rnn(clean_csv_file=CLEAN_GUINEA_DATA_PATH, da
     print ("finished processing data to numpy.")
 
 
+def find_first_and_last_entry(rows):
+    rows = sorted(rows, key=lambda x: (x[4])) # sort by date
+    return int(rows[0][4]), int(rows[-1][4])
+
+
+
+def find_first_and_last_shared_days(data_dict_by_province):
+
+    first_shared_day = -sys.maxint - 1
+    last_shared_day = sys.maxint
+    for province, rows in data_dict_by_province.iteritems():
+        first_day, last_day = find_first_and_last_entry(rows)
+        print "Province: {} \t first day: {} \t last day: {}".format(province, first_day, last_day)
+        first_shared_day = max(first_shared_day, first_day)
+        last_shared_day = min(last_shared_day, last_day)
+    return first_shared_day, last_shared_day
+
+
+def create_data_for_extrapolation_aligned_by_time(clean_csv_file=CLEAN_GUINEA_DATA_PATH, dataset_name="guinea", num_timesteps=25, case_type="confirmed cases"):
+    data_dict_by_province = get_data_dict_from_clean_csv(clean_csv_file=clean_csv_file, dataset_name=dataset_name,
+                                                         case_type=case_type)
+
+
+    provinces, data, labels =[], [], []
+
+    new_data_dict_by_province = defaultdict(list)
+    for province, rows in data_dict_by_province.iteritems():
+        rows = sorted(rows, key=lambda x: (x[4]))  # sort by date
+        first_day, last_day = find_first_and_last_entry(rows)
+        if first_day > 250:
+            print "first day too late. Skipping province {}".format(province)
+            continue
+        new_data_dict_by_province[province] = rows
+
+    first_shared_day, last_shared_day = find_first_and_last_shared_days(new_data_dict_by_province)
+
+
+
+
+    for province, rows in new_data_dict_by_province.iteritems():
+        rows = sorted(rows, key=lambda x: int(x[4]))  # sort by date
+        # print rows
+        # selected_rows.append(filter(lambda x: int(x[4]) == first_shared_day, rows)[0])
+        # selected_rows.append(rows.find(lambda x: int(x[4]) == first_shared_day))
+
+        i = (m for m, r in enumerate(rows) if int(r[4]) >= 250).next()
+
+        print rows[i]
+        # since we are predicting the next timestep, we don't use the features from the last
+        # timestep, since we wouldn't know what the prediction would be.
+        sample_across_timesteps = []
+        prev_num_cases = 0
+        for j in xrange(num_timesteps):
+            num_cases, rel_lat, rel_lon = int(rows[i + j][3]), float(rows[i + j][5]), float(rows[i + j][6])
+            if num_cases < prev_num_cases:
+                # to account for odd numbers in the data, where the number of cases decreases
+                num_cases = prev_num_cases
+            prev_num_cases = num_cases
+            sample_across_timesteps.append(np.array([num_cases, rel_lat, rel_lon]))
+
+        provinces.append(province)
+        data.append(np.array(sample_across_timesteps))
+        labels.append(int(rows[i + num_timesteps][3]))
+
+    dataset = data, labels, provinces
+
+
+
+    np.save("../data/preprocessed/" + dataset_name + "_" + str(num_timesteps) + "_for_extrapolation.npy", dataset)
+    print ("finished processing data for extrapolation.")
+
+    data, labels, provinces = np.load("../data/preprocessed/" + dataset_name + "_" + str(num_timesteps) + "_for_extrapolation.npy")
+
+    print data
+    print labels
+    print provinces
+
+
+
+
+
+
+
+
 # def create_data_with_one_row_per_province_aligned_in_time(csv_file, raw_data_dict, num_timesteps=25, case_type='confirmed cases'):
 #     raw_data_dict, num_timesteps, provinces_latlon, mean_lat, mean_lon, scale_lat, scale_lon = \
 #         extract_data_from_csv(csv_file, provinces_latlon, num_timesteps=num_timesteps, case_type=case_type)
@@ -157,6 +245,8 @@ if __name__ == "__main__":
     # for country, country_file in zip(COUNTRIES, COUNTRIES_DATA_PATHS):
     #     clean_data_convert_dates_make_normalize_lat_lon(country_file, country)
 
-    for clean_country_file, country in zip(COUNTRIES_CLEAN_DATA_PATHS, COUNTRIES):
-        convert_clean_csv_to_numpy_for_rnn(clean_country_file, dataset_name=country)
+    # for clean_country_file, country in zip(COUNTRIES_CLEAN_DATA_PATHS, COUNTRIES):
+    #     convert_clean_csv_to_numpy_for_rnn(clean_country_file, dataset_name=country)
+    #
 
+    create_data_for_extrapolation_aligned_by_time()
