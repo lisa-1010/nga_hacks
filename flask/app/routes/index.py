02cb958f87acecaa198447d560758f296cbf3b79
gnedivad <gnedivad@gmail.com>
2016-08-06 20:44:50 -0700

    Fixed routing

--- flask/app/routes/index.py
+++ flask/app/routes/index.py
@@ -5,4 +5,8 @@ from app import app
 
 @app.route('/', methods=['GET'])
 def index():
+  return render_template('index.html')
+
+@app.route('/tmp', methods=['GET'])
+def tmp():
   return render_template('tmp.html')
